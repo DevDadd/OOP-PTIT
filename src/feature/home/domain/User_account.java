b71f73201bc7280eeab5a05635d43fa3e252401a
tien15092005 <tranquangtien15092005@gmail.com>
2025-10-01 23:40:52 +0700

    Update User_account.java
    demo method

--- src/feature/home/domain/User_account.java
+++ src/feature/home/domain/User_account.java
@@ -9,21 +9,45 @@ public class User_account {
     private String phone;
     private String password;
 
+    private boolean isLoggedIn = false;
+
     // Constructor
     public User_account() {
     }
 
+    public User_account(int id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
     // Methods
     public boolean login(String email, String password) {
-        // TODO
+        if (this.email != null && this.email.equals(email) && this.password.equals(password)) {
+            isLoggedIn = true;
+            System.out.println("Login successful! Welcome " + name);
+            return true;
+        }
+        System.out.println("Login failed!");
         return false;
     }
 
     public void logout() {
-        // TODO
+        if (isLoggedIn) {
+            isLoggedIn = false;
+            System.out.println("User " + name + " logged out.");
+        } else {
+            System.out.println("No user is currently logged in.");
+        }
     }
 
     public void sign_up(String name, String email, String phone, String password) {
-        // TODO
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        System.out.println("User registered successfully: " + name);
     }
 }
