1874ffb1a8b0919f7d3a0d68b2c48028c46e8701
tien15092005 <tranquangtien15092005@gmail.com>
2025-10-01 23:39:21 +0700

    Update Personal_lists.java
    demo methods

--- src/feature/home/domain/Personal_lists.java
+++ src/feature/home/domain/Personal_lists.java
@@ -10,8 +10,42 @@ public class Personal_lists {
     public Personal_lists() {
     }
 
+    public Personal_lists(String time, String movie, double cost) {
+        this.time = time;
+        this.movie = movie;
+        this.cost = cost;
+    }
+
+    // Getter & Setter
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
     // Methods
     public void show() {
-        // TODO
+        System.out.println("Personal list:");
+        System.out.println("Movie: " + movie);
+        System.out.println("Time: " + time);
+        System.out.println("Cost: " + cost + " VND");
     }
 }
