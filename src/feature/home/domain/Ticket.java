74631d139d6db8b4eb009820f796419a5d753be1
tien15092005 <tranquangtien15092005@gmail.com>
2025-10-01 23:40:20 +0700

    Update Ticket.java
    demo methods

--- src/feature/home/domain/Ticket.java
+++ src/feature/home/domain/Ticket.java
@@ -13,8 +13,42 @@ public class Ticket {
     public Ticket() {
     }
 
+    public Ticket(String showtime, String seat, String room, String cinema, double cost) {
+        this.showtime = showtime;
+        this.seat = seat;
+        this.room = room;
+        this.cinema = cinema;
+        this.cost = cost;
+    }
+
+    // Getter & Setter
+    public String getShowtime() {
+        return showtime;
+    }
+
+    public String getSeat() {
+        return seat;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public String getCinema() {
+        return cinema;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
     // Methods
     public void show() {
-        // TODO
+        System.out.println("Ticket info:");
+        System.out.println("Cinema: " + cinema);
+        System.out.println("Room: " + room);
+        System.out.println("Seat: " + seat);
+        System.out.println("Showtime: " + showtime);
+        System.out.println("Cost: " + cost + " VND");
     }
 }
